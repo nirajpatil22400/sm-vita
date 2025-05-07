6212baeca7a92fd6d08153777667354262a45fc7
h9nirajpatil <h9.nirajpatil@outlook.com>
2025-05-07 10:41:24 +0530

    "textchange added"

---
+++ textdatachange/src/App.jsx
@@ -0,0 +1,16 @@
+import MyInput from "./Components/MyInput";
+import MyCheckbox from "./Components/MyCheckbox";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Input and Checkbox Example</h1>
+      <MyInput />
+      <hr />
+      <MyCheckbox />
+    </div>
+  );
+}
+
+export default App;
